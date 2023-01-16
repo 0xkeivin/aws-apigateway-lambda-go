da202624a95666d34d8e0e043f151a393263fe4c
0xkeivin <keivincatwork@gmail.com>
2023-01-16 12:14:38 +0800

    added user_notes table

--- terraform_infra/4.dynamodb.tf
+++ terraform_infra/4.dynamodb.tf
@@ -34,3 +34,50 @@ resource "aws_dynamodb_table_item" "token2" {
 }
 ITEM
 }
+
+// user-notes
+
+resource "aws_dynamodb_table" "user_notes" {
+  name           = "user-notes"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key = "user"
+  range_key = "create_date"  
+  
+  attribute {
+    name = "user"
+    type = "S"
+  }
+  attribute {
+    name = "create_date"
+    type = "S"
+  }
+}
+resource "aws_dynamodb_table_item" "user1" {
+  table_name = aws_dynamodb_table.user_notes.name
+  hash_key   = aws_dynamodb_table.user_notes.hash_key
+  range_key = aws_dynamodb_table.user_notes.range_key
+
+  item = <<ITEM
+{
+  "user": {"S": "useremail1@gmail.com"},
+  "create_date": {"S": "2023-01-01T00:00:00Z"},
+  "text": {"S": "sample note1"}
+}
+ITEM
+}
+
+resource "aws_dynamodb_table_item" "user2" {
+  table_name = aws_dynamodb_table.user_notes.name
+  hash_key   = aws_dynamodb_table.user_notes.hash_key
+  range_key = aws_dynamodb_table.user_notes.range_key
+
+  item = <<ITEM
+{
+  "user": {"S": "useremail2@gmail.com"},
+  "create_date": {"S": "2023-01-02T00:00:00Z"},
+  "text": {"S": "sample note2"}
+}
+ITEM
+}
